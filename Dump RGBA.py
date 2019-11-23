fe5d3397dafb51cedcff5781f35364301b9a7e41
Moonling <52844176+VelouriasMoon@users.noreply.github.com>
2019-11-23 13:03:47 -0400

    Add files via upload

--- Dump RGBA.py
+++ Dump RGBA.py
@@ -9,9 +9,9 @@ for infile in sys.argv[1:]:
 		img.mode = 'RGBA'
 
 		# Suppress specific bands (e.g. (255, 120, 65) -> (0, 120, 0) for g)
-		r = [(d[0], 0, 0, 255) for d in data]
-		g = [(0, d[1], 0, 255) for d in data]
-		b = [(0, 0, d[2], 255) for d in data]
+		r = [(d[0], d[0], d[0], 255) for d in data]
+		g = [(d[1], d[1], d[1], 255) for d in data]
+		b = [(d[2], d[2], d[2], 255) for d in data]
 		a = [(d[3], d[3], d[3], 255) for d in data]
 
 		img.putdata(r)
